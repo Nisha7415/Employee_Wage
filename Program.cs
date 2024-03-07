36b35c9712b38cc2a8611e3f8ed1ed4f5a389dfc
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:30:51 +0530

    UC2 Commited

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//     Calculate daily employee wage.
+
+using System;
+
+namespace UC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck= random.Next(0,2);
+            Console.WriteLine(empcheck);
+            if (empcheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage is :- " + empwage);
+        }
+    }
+}
