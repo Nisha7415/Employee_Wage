f3eda9ac6352b8080b75f77d267d55c645735bbf
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:37:27 +0530

    UC3 Commited

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+//Add part time employee and wage.
+
+
+using System;
+using System.ComponentModel.Design;
+
+namespace UC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            Console.WriteLine("Employeecheck is :- " + empcheck);
+            if(empcheck==IS_PART_TIME)
+            {
+                emphrs = 4;
+            }
+            else if(empcheck==IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage is :- " + empwage);
+        }
+    }
+}
