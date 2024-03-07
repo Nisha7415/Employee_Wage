72e87e428ea3ec1f3142892ccf11007f9b432aac
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:27:47 +0530

    UC1 Commited

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Check Employee is present or absent.
+
+
+using System;
+
+
+namespace employeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck=random.Next(0,2);
+            if (empcheck == IS_FULL_TIME) 
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
