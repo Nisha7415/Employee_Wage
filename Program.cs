d73d12f0f412c353d0202e47f468d6d6cfb14c7f
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:49:49 +0530

    UC7 Commited

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UC_7
+{
+      public class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOUR_IN_MONTH = 10;
+
+        public static int computeEmpwage()
+
+        {
+            int emphrs = 0;
+            int totalemphrs = 0;
+            int totalWorkingdays = 0;
+            while (totalemphrs <= MAX_HOUR_IN_MONTH && totalWorkingdays <= NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingdays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        emphrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        emphrs = 8;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                totalemphrs += emphrs;
+                Console.WriteLine("Day#   " + totalWorkingdays + "emp hrs : " + emphrs);
+            }
+            int totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total employee wage :- " + totalempwage);
+            return totalempwage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpwage();
+        }
+    }
+}
